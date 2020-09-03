b9dab8060e89a1decca32d7fe9ebfb02ad14eec4
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-09-03 23:15:27 +0530

    Add project files.

---
+++ interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+
+    interface ICustomer
+    {
+        void Print();
+    }
+
+    class Customer : ICustomer
+    {
+        public void Print()
+        {
+            Console.WriteLine("Interface Print method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer C1 = new Customer();
+            C1.Print();
+            Console.ReadLine();
+
+        }
+    }
+}
